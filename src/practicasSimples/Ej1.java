0eb22821d1ab62c649bd14178ba2c687587a1832
Scatm <Scatm@DESKTOP-G1QJD38>
2021-04-28 15:03:11 -0300

    Primer ejemplo.

---
+++ src/practicasSimples/Ej1.java
@@ -0,0 +1,12 @@
+package practicasSimples;
+//Mostrar por consola un "Hola" seguido de una String que contenga un nombre.
+public class Ej1 {
+
+	public static void main(String[] args) {
+		
+		String nombre = "Pepe"; //Int de String que contiene el nombre Pepe.
+		System.out.println("Hola " + nombre + "."); // Escribe por consola una frase compuesta.
+
+	}
+
+}
